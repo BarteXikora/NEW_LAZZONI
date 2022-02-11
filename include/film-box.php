14e9ae7471b45fa530340aa9104c21e85e9542b5
barteXikora <sbartek50@gmail.com>
2022-02-11 14:51:44 +0100

    Film window and table styles added

--- include/film-box.php
+++ include/film-box.php
@@ -1,18 +1,20 @@
 <div class="film-box-container d-none" data-default="<?php if (isset($_GET['v'])) echo $_GET['v'] ?>"> 
     <div class="film-box">
-        <div class="film-box-close-box">
-            <button class="film-box-close">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/icon-close.png" alt="Zamknij okno." class="p-2">
-            </button>
+        <div class="film-box-bar p-0 mb-3">
+            <div class="film-box-close-box">
+                <button class="film-box-close">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/icon-close.png" alt="Zamknij okno." class="p-2">
+                </button>
+            </div>
         </div>
-        <div class="px-5 pt-2 pb-2">
+        <div class="px-5 px-md-3 pt-2 pb-2">
             <iframe 
                 id="yt-film-iframe" 
                 src=""
                 class="yt-film-iframe"
                 allowfullscreen>
             </iframe>
-            <h3 id="film-title" class="mt-4"></h3>
+            <h3 id="film-title" class="mt-4 px-4"></h3>
         </div>
     </div>
 </div>
