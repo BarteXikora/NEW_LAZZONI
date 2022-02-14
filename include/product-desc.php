129cedfd1f77a2daa80d2ca56d694e1d0014a41d
barteXikora <sbartek50@gmail.com>
2022-02-14 09:04:58 +0100

    Product group description moved to include directory

---
+++ include/product-desc.php
@@ -0,0 +1,64 @@
+<?php if ($args['group'] == 'test') { // ============================================================?> 
+
+    <div class="col-12 mb-5 text-center text-md-left">
+        <h1 class="h4 font-title mb-3">
+            <a href="tel:+48504262401">
+                GŁOWICE WIERTARSKIE &mdash; ZAMÓW TERAZ +48 504 262 401
+            </a>
+        </h1>
+
+        <p>
+            Jesteśmy polskim producentem głowic wiertarskich. Zaufało nam już ponad 1000 firm. 
+            Nasze produkty podbijają również rynek zagraniczny. Podczas współpracy z klientem 
+            dokładamy wszelkiej staranności, aby każda nasza głowica wiertarska była niezawodna. 
+            Ponadto jesteśmy w stanie wyprodukować głowicę w bardzo krótkim czasie.
+        </p>
+
+        <div class="collapse m-0" id="read-more">
+            <p>
+                Produkcja głowic wiertarskich odbywa się z zachowaniem wszelkich norm 
+                konstrukcyjnych oraz na najwyższym poziomie tolerancji. Głowica wiertarska składa 
+                się z solidnych podzespołów oraz stalowego korpusu, co przekłada się na żywotność 
+                i bezawaryjność. Kontrola jakości wraz z zapisaniem jej historii na każdym etapie 
+                produkcji sprawia, że nasze głowice wiertarskie charakteryzują się najwyższą 
+                wydajnością. Szeroki wybór głowic wiertarskich pozwala na idealne dopasowanie 
+                urządzenia do potrzeb klienta. Realizujemy indywidualne zamówienia, zapewniając 
+                elastyczność na każdym etapie współpracy.
+            </p>
+
+            <p>W naszej ofercie znajdziemy:</p>
+            <ul>
+                <li>Głowice wiertarskie (głowice wielowrzecionowe)</li>
+                <li>Głowice na zamówienie</li>
+                <li>Głowice liniowe</li>
+                <li>Głowice puszkowe</li>
+            </ul>
+
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-3">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/glowice-wiertarskie/gw-0.png" alt="" class="img-fluid p-5 p-lg-2">
+                </div> 
+
+                <div class="col-12 col-md-6 col-lg-3">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/glowice-wiertarskie/gw-1.png" alt="" class="img-fluid p-5 p-lg-2">
+                </div> 
+
+                <div class="col-12 col-md-6 col-lg-3">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/glowice-wiertarskie/gw-2.png" alt="" class="img-fluid p-5 p-lg-2">
+                </div> 
+
+                <div class="col-12 col-md-6 col-lg-3">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/glowice-wiertarskie/gw-3.png" alt="" class="img-fluid p-5 p-lg-2">
+                </div> 
+            </div>
+        </div>
+
+        <div class="text-center">
+            <button class="button-read-more py-2 px-4 mt-2" type="button" data-toggle="collapse"
+                data-target="#read-more">
+                Czytaj dalej
+            </button>
+        </div>
+    </div>
+
+<?php } else if ($args['group'] == '') {} // ============================================================?>
