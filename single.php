bb701faf9954c1ad93f83a49dd383f771595a035
barteXikora <sbartek50@gmail.com>
2022-02-14 08:43:20 +0100

    Some styles for single post added. Button in single fixed

--- single.php
+++ single.php
@@ -2,7 +2,7 @@
 
 <!-- MAIN CONTENT -->
 <main class="container single-container my-5">
-    <div class="row my-5 pt-3">
+    <div class="row my-5 <?php if (get_post_type(get_the_ID()) == 'post') echo 'pt-5'; else echo 'pt-2' ?>">
         <?php if (get_post_type(get_the_ID()) == 'news') { ?>
             <div class="col-12 pt-5 text-center text-md-left">
                 <h1 class="title"><?php the_title(); ?></h1>
@@ -16,6 +16,8 @@
         <div class="col-12 text-center mt-5">
             <?php if (get_post_type(get_the_ID()) == 'news') { ?>
                 <a href="<?php echo get_page_link(get_page_by_path('news')); ?>" class="button button-prim">Wróć do aktualności</a>
+            <?php } else if (get_post_type(get_the_ID()) == 'post') { ?>
+                <a href="<?php echo get_page_link(get_page_by_path('products')); ?>" class="button button-prim">Wróć na listę produktów</a>
             <?php } else { ?>
                 <a href="<?php echo home_url(); ?>" class="button button-prim">Wróć na stronę główną</a>
             <?php } ?>
