748bf37f2ff4af1e0019f72cb3b5e5049360ef8b
barteXikora <sbartek50@gmail.com>
2022-02-15 09:53:16 +0100

    Description for wiertarki page added. Links in kafelki fixed

--- include/product-desc.php
+++ include/product-desc.php
@@ -61,4 +61,85 @@
         </div>
     </div>
 
-<?php } else if ($args['group'] == '') {} // ============================================================?>
+<?php } else if ($args['group'] == 'wiertarki') {  // ============================================================?>
+
+    <div class="col-12 mb-5 text-center text-md-left">
+        <h1 class="h4 font-title mb-3">
+            WIERTARKA WIELOWRZECIONOWA, PRZELOTOWA
+        </h1>
+
+        <p>
+            Wiertarki wielowrzecionowe Lazzoni Group zostały stworzone z myślą zarówno o potrzebach 
+            odbiorców przemysłowych, jak i małych zakładów stolarskich to maszyny, których nie trzeba 
+            nikomu przedstawiać. Jak widać nasze wiertarki przelotowe w szybkim tempie podbiły rynek 
+            Europejski.
+        </p>
+
+        <div class="collapse m-0" id="read-more">
+            <p>
+                W przeciągu kilku lat doposażyliśmy mnóstwo zakładów stolarskich, a co za tym idzie 
+                coraz bardziej właśnie to nasze wiertarki przelotowe zostają wybierane spośród 
+                wszystkich dostępnych na rynku. Każda wiertarka przelotowa została dokładnie 
+                przetestowana tak, aby klient mógł pracować bez żadnych zastrzeżeń. Najwyższa jakość 
+                wiertarek, wysokiej klasy podzespoły, ogromne doświadczenie oraz współpraca z 
+                producentami z całej Europy sprawiły, że wiertarki wielowrzecionowe tej marki są w 
+                stanie zaspokoić nawet najbardziej rygorystyczne oczekiwania klientów.
+            </p><p>
+                Pierwsza wiertarka wielowrzecionowa oraz wiertarka przelotowa Lazzoni Group została 
+                stworzona w 2000 r. Doświadczenie, które zdobyliśmy przez ten okres czasu pozwala nam 
+                na zastosowanie różnych rozwiązań w tematyce wiertarek wielowrzecionowych. Możemy 
+                śmiało powiedzieć, że tworzymy silną grupę produktów takich jak: wiertarki przelotowe 
+                / wiertarki wielowrzecionowe / wiertarki CNC / wiertarki na zamówienie(możliwość 
+                szerokiej personalizacji) dla zakładów stolarskich oraz przemysłu. ponadto Współpraca 
+                z różnymi producentami z Europy, Turcji oraz Azji pozwoliła nam jeszcze bardziej 
+                udoskonalić każdą wiertarkę przelotową.
+            </p><p>
+                Praktycznie każda nasza wiertarka wielowrzecionowa oraz wiertarka przelotowa posiada 
+                możliwość konfiguracji pod szczególne wymagania klienta. „Wyposażenie w dodatkowe 
+                jednostki wiercące, zamontowanie spersonalizowanej głowicy wiertarskiej, powrót 
+                elementu do operatora, zwiększenie pola roboczego wiertarki wielowrzecionowej” 
+                są to przykładowe konfiguracje, które jesteśmy w stanie wykonać. Dodatkowym naszym 
+                atutem jest fakt, iż posiadamy Show room liczący ponad 50 maszyn dostępnych od ręki 
+                oraz kolejne kilkadziesiąt z krótkim czasem realizacji. Ponadto jako jeden z czołowych 
+                producentów wiertarek nieprzerwanie prowadzimy badania nad rozwojem maszyn. Dzięki 
+                czemu, wiertarki przelotowe oraz wielowrzecionowe naszej produkcji posiadają szerokie 
+                możliwości konfiguracyjne.
+            </p><p>
+                Lazzoni Group posiada własny serwis składający się z wielu doświadczonych osób. Każdy 
+                nasz serwisant posiada odpowiednią wiedzę i doświadczenie w tematyce wiertarek 
+                przelotowych a co za tym idzie jesteśmy w stanie zareagować w bardzo szybkim tempie.
+            </p>
+
+            <h2>WIERTARKA CNC</h2>
+            <p>
+                Wiertarki CNC firmy Lazzoni Group przeznaczone są dla dużych, średnich oraz małych 
+                zakładów stolarskich. W naszej ofercie znajdą Państwo szereg sprawdzonych rozwiązań, 
+                dzięki którym śmiało możemy powiedzieć, że nasze Wiertarki CNC sprostają każdemu 
+                zadaniu.
+            </p><p>
+                Nasze pierwsze Wiertarki CNC zostały stworzone z myślą o fabrykach, które znacznie chcą 
+                przyspieszyć swoją produkcję. Praktycznie każda Wiertarka CNC posiada możliwość 
+                szerokiej konfiguracji pod szczególne wymagania klientów. Wyposażenie w dodatkowe 
+                jednostki wiercące CNC, zamontowanie spersonalizowanej głowicy wiertarskiej, 
+                zwiększenie pola roboczego wiertarki CNC to tylko kilka przykładów o które możemy 
+                poszerzyć nasze urządzenia. Dodatkowo nasze wiertarki CNC mogą zostać doposażone w 
+                automatyczny załadunek i rozładunek elementu. Urządzenia z serii EXPERT oraz PRO mogą 
+                być łączone w zautomatyzowane linie wiertarskie, co pozwala nam znacząco przyspieszyć 
+                produkcję.
+            </p><p>
+                Zapewniamy również projektowanie ciągów technologicznych dla wiertarek CNC pod względem
+                wydajności, efektywności oraz jakości. Dodatkowo oferujemy profesjonalny serwis 
+                gwarancyjny, obsługę pogwarancyjną oraz fachowe doradztwo z zakresu zakupu wiertarek 
+                wielowrzecionowych.
+            </p>
+        </div>
+
+        <div class="text-center">
+            <button class="button-read-more py-2 px-4 mt-2" type="button" data-toggle="collapse"
+                data-target="#read-more">
+                Czytaj dalej
+            </button>
+        </div>
+    </div>
+
+<?php } ?>
